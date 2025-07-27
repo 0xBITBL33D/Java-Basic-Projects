9305eeed14325c9e26b5762be917132c7eb5388d
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-27 09:11:41 -0400

    Update BasicCalc.java

--- BasicCalc.java
+++ BasicCalc.java
@@ -41,16 +41,22 @@ public class BasicCalc {
     static float divide(int num1, int num2) {
         float answer = num1 / num2;
         if(num1 == 0 || num2 == 0) {
-            System.err.println("Division by Zero is now allowed");
+            System.err.println("Division by Zero is not allowed");
             return 0;
         }
         return answer;
     }
 
-
+    static void calcLoop(){
+        
+    }
 
     public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
+    
+    System.out.print("Hellow")
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
     }
 }
