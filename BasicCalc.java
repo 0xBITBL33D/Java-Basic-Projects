31fd1662fe8596627c9bd3cee626105cb913aa1b
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-27 09:10:11 -0400

    Update BasicCalc.java

--- BasicCalc.java
+++ BasicCalc.java
@@ -23,8 +23,28 @@ import java.util.Scanner;
 
 public class BasicCalc {
     
-    static int add(int a, int b) {
-        
+    static int add(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+
+    static int subtract(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+
+    static int multiply(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+
+    static float divide(int num1, int num2) {
+        float answer = num1 / num2;
+        if(num1 == 0 || num2 == 0) {
+            System.err.println("Division by Zero is now allowed");
+            return 0;
+        }
+        return answer;
     }
 
 
