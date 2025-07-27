20cf78d997238354b7a584d5678c7ca795219930
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-27 08:28:20 -0400

    Create BasicCalc.java

---
+++ BasicCalc.java
@@ -0,0 +1,36 @@
+/*
+ *     // 1. Say Hello
+        // 2. Ask for first number
+        // 3. Ask for second number
+
+        // 4. Call methods for add/subtract/multiply/divide
+        // 5. Print the results
+
+        scanner.close();
+    }
+
+    // 6. Make static methods:
+    //    add(int a, int b)
+    //    subtract(int a, int b)
+    //    multiply(int a, int b)
+    //    divide(int a, int b)
+}
+ */
+
+import java.util.Scanner;
+
+
+
+public class BasicCalc {
+    
+    static int add(int a, int b) {
+        
+    }
+
+
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    }
+}
