934b61d8b4f49e291e684f7871f88598f1cb6c4f
Sho Nakamura <0xbitbleed@gmail.com>
2025-07-27 09:32:33 -0400

    Update BasicCalc.java

--- BasicCalc.java
+++ BasicCalc.java
@@ -47,16 +47,42 @@ public class BasicCalc {
         return answer;
     }
 
-    static void calcLoop(){
+    static void calcLoop(Scanner sc){
         
-    }
+        while(true) {
+            System.out.print("Hello, Welcome to the Basic Calc");
 
+            int num1 = Integer.MAX_VALUE;
+            int num2 = Integer.MAX_VALUE;
+
+            try{
+                System.out.println("Enter first number: ");
+                if(sc.hasNextInt()) {
+                    num1 = sc.nextInt();
+                }
+
+                System.out.println("Enter second number: ");
+                if(sc.hasNextInt()) {
+                    num2 = sc.nextInt();
+                } 
+            }
+            catch(NumberFormatException e){
+                System.err.println("Invalid Input!");
+                continue;
+            }
+
+            System.out.printf("%d + %d = %d", num1, num2, add(num1, num2));
+            System.out.printf("%d - %d = %d", num1, num2, subtract(num1, num2));
+            System.out.printf("%d * %d = %d", num1, num2, multiply(num1, num2));
+            System.out.printf("%d / %d = %d", num1, num2, divide(num1, num2));
+            
+            
+
+        }
+    }
     public static void main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
-    
-    System.out.print("Hellow")
-    int num1 = sc.nextInt();
-    int num2 = sc.nextInt();
+
     }
 }
